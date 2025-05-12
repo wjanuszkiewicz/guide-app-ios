232a44e242264598fdf1ea0fd6de85a0f6155898
wjanuszkiewicz <wjanuszkiewicz5@gmail.com>
2025-05-12 19:23:02 +0300

    Initial Commit

---
+++ guide-app-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guide-app-ios
+//
+//  Created by Владислав Янушкевич on 12.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guide-app-ios/guide_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  guide_app_iosApp.swift
+//  guide-app-ios
+//
+//  Created by Владислав Янушкевич on 12.05.25.
+//
+
+import SwiftUI
+
+@main
+struct guide_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
